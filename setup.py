a85ca923dcee82387807cda00be8286d75417344
Tim Felgentreff <tim.felgentreff@oracle.com>
2022-04-22 12:48:10 +0200

    add copyright notices

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #------------------------------------------------------------------------------
 # Copyright (c) 2013-2020, Nucleic Development Team.
+# Copyright (c) 2022, Oracle and/or its affiliates.
 #
 # Distributed under the terms of the Modified BSD License.
 #
