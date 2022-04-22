76507c93a91a7ad57abc817761af3b31d5c3758c
Tim Felgentreff <tim.felgentreff@oracle.com>
2022-04-22 13:25:54 +0200

    add copyright notice

--- py/tests/conftest.py
+++ py/tests/conftest.py
@@ -1,3 +1,11 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2013-2020, Nucleic Development Team.
+# Copyright (c) 2022, Oracle and/or its affiliates.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+#------------------------------------------------------------------------------
 import pytest
 from hpy.debug.pytest import LeakDetector
 
